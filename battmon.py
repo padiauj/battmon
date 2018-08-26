5e9c4e55b4dc4c174e8d86934f833ad522e2e6e4
Umesh Padia <padiauj@users.noreply.github.com>
2018-08-26 15:09:20 -0400

    Added helper functions for logging and graphing, adde PyQt4 interface for interacting with the graph.

--- battmon.py
+++ battmon.py
@@ -24,7 +24,7 @@
 # Kernel specifications for /sys/class/power_supply: 
 # https://www.kernel.org/doc/Documentation/ABI/testing/sysfs-class-power
 
-# TODO: tolerate missing information
+# TODO: tolerate missing information, and error correcting code 
 
 import os
 import sys
@@ -35,88 +35,164 @@ import argparse
 import matplotlib.pyplot as plt
 import numpy as np
 
+from PyQt4 import QtGui
+from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.backends.backend_qt4agg import NavigationToolbar2QT as NavigationToolbar
+from matplotlib.figure import Figure
 
 POWER_SUPPLY_PATH = "/sys/class/power_supply/"
 LOG_PATH = "/var/log/battmon"
 
 millis_time = lambda: int(round(time.time() * 1000))
 
-
+# Reads the entire contents of the file specified. If no such file exists, an
+# empty string is returned. The contents is stripped of whitespace at the ends.
 def read_path(path):
     if (not os.path.isfile(path)):
         return ""
     with open(path, 'r') as f:
         return f.read().strip() 
 
-def log_battery_state():
-    # if logging dir does not exist, create it. 
-    if not os.path.exists(LOG_PATH):
-        os.mkdir(LOG_PATH)
-
+# Reads the battery/ies information from POWER_SUPPLY_PATH according to the 
+# kernel specifications and returns a nested dictionary with format:
+# { "<manufacturer>_<model_name>_<serial_number>" : { "capacity" : <capacity>
+# , "status" : <status>}}
+def get_battery_states():
     # get all supply names
     supplies = list(os.walk(POWER_SUPPLY_PATH))[0][1]
-
+    battery_data_files = ["capacity", "status"]
+    battery_name_files = ["manufacturer", "model_name", "serial_number"]
+    battery_states = {}
     for supply in supplies:
         supply_path = os.path.join(POWER_SUPPLY_PATH, supply)
         type = read_path(os.path.join(supply_path, "type"))
         if (type == "Battery"):
-            # voltage_now = read_path(os.path.join(supply_path, "voltage_now"))
-            # voltage_max = read_path(os.path.join(supply_path, "voltage_max"))
-            # voltage_min = read_path(os.path.join(supply_path, "voltage_min"))
+            battery_id =  "_".join([read_path(os.path.join(supply_path, bnf)) for bnf in battery_name_files])
+            battery_states[battery_id] = {}
+            for bdf in battery_data_files:
+                battery_states[battery_id][bdf] = read_path(os.path.join(supply_path, bdf))
+    return battery_states
 
-            capacity = read_path(os.path.join(supply_path, "capacity"))
+# Logs the information from get_battery_states() to individual log files for
+# each battery under LOG_PATH
+def log_battery_state():
+    # if logging dir does not exist, create it. 
+    if not os.path.exists(LOG_PATH):
+        os.mkdir(LOG_PATH)
+    states = get_battery_states() 
+    time = str(millis_time())
+    for battery in states:
+        print(states[battery])
+        fields = [time, states[battery]["status"][0].upper(), states[battery]["capacity"]]
+        with open(os.path.join(LOG_PATH, battery + ".log"), 'a') as log:
+            writer = csv.writer(log)
+            writer.writerow(fields)
 
-            # according to spec, possible statuses are: Unknown, Charging, 
-            # Discharging, Not charging, Full . We will store the first letter
-            # (capital) 
-
-            status = read_path(os.path.join(supply_path, "status"))[0].upper() 
-            manufacturer = read_path(os.path.join(supply_path, "manufacturer"))
-            model_name = read_path(os.path.join(supply_path, "model_name"))
-            serial_number = read_path(os.path.join(supply_path, "serial_number"))
-
-            time = str(millis_time())
-            battery_id = manufacturer + "_" + model_name + "_" + serial_number
-
-            fields = [time, status, capacity]
-            with open(os.path.join(LOG_PATH, battery_id + ".log"), 'a') as log:
-                writer = csv.writer(log)
-                writer.writerow(fields)
-
-def graph_battery():
+# Retrieves battery history from the logfiles in LOG_PATH
+# Recall that CSV log format is: Time (millis), Status, Capacity. Battery ID 
+# derived from the log name, stored in LOG_PATH/<battery_id>.log
+def get_battery_history(offset):
     logfiles = [f for f in os.listdir(LOG_PATH) if os.path.isfile(os.path.join(LOG_PATH, f))]
-    series = []
+    history = {}
+    cutoff = millis_time() - offset
     for logfile in logfiles:
         reader = csv.reader(open(os.path.join(LOG_PATH, logfile), 'r'))
         time = []
         capacity = []
         for row in reader:
-            time.append(datetime.datetime.fromtimestamp(int(row[0]) / 1000.0))
-            capacity.append(int(row[2]))
-        series.append([os.path.basename(logfile), time,capacity])
-        title = max(time)
-    for s in series:
-        plt.plot(s[1], s[2], label=s[0])
-    plt.xlabel("Date")
-    plt.ylabel("Charge (%)")
-    plt.title("Last updated: " + str(title))
-    plt.legend()
-    plt.show()
+            timestamp = int(row[0])
+            if (timestamp >= cutoff):
+                # convert UNIX millisecond timestamp to datetime object
+                time.append(datetime.datetime.fromtimestamp(timestamp / 1000.0))
+                capacity.append(int(row[2]))
+        history[os.path.splitext(logfile)[0].strip()] = (time, capacity)
+    return history
 
+# Battery Graph GUI (PyQt4)
+class Window(QtGui.QDialog):
+    def __init__(self, parent=None):
+        MILLIS_IN_DAY = 86400000
+        super(Window, self).__init__(parent)
+
+        self.setWindowTitle("Battmon") 
+
+        # a figure instance to plot on
+        self.figure = Figure()
+
+        # this is the Canvas Widget that displays the `figure`
+        # it takes the `figure` instance as a parameter to __init__
+        self.canvas = FigureCanvas(self.figure)
+
+        # this is the Navigation widget
+        # it takes the Canvas widget and a parent
+        self.toolbar = NavigationToolbar(self.canvas, self)
+
+        # set the layout
+        layout = QtGui.QVBoxLayout()
+
+        timeframe_gbox = QtGui.QGroupBox("Time") 
+        self.day = QtGui.QRadioButton("Day")
+        self.week = QtGui.QRadioButton("Week")
+        self.month = QtGui.QRadioButton("Month")
+        self.all = QtGui.QRadioButton("All")
+
+        self.day.clicked.connect(lambda: self.plot(MILLIS_IN_DAY))
+        self.week.clicked.connect(lambda: self.plot(7*MILLIS_IN_DAY))
+        self.month.clicked.connect(lambda: self.plot(30*MILLIS_IN_DAY))
+        self.all.clicked.connect(lambda: self.plot(millis_time()))
+
+
+        timeframe_hbox = QtGui.QHBoxLayout() 
+        timeframe_hbox.addWidget(self.day)
+        timeframe_hbox.addWidget(self.week)
+        timeframe_hbox.addWidget(self.month)
+        timeframe_hbox.addWidget(self.all)
+        timeframe_gbox.setLayout(timeframe_hbox)
+
+        # get current battery states
+        states = get_battery_states()
+        for battery in states:
+            label = "Battery: " + battery
+            label += " (" + states[battery]["capacity"] + "%, " + states[battery]["status"] + ")"
+            layout.addWidget(QtGui.QLabel(label))
+        layout.addWidget(timeframe_gbox)
+        layout.addWidget(self.toolbar)
+        layout.addWidget(self.canvas)
+        self.setLayout(layout)
+
+        self.all.setChecked(True)
+        self.plot(millis_time())
+
+    def plot(self, offset):
+        # clear plot
+        self.figure.clear() 
+        ax = self.figure.add_subplot(111)
+        # get battery data from logs
+        history = get_battery_history(offset)
+        # plot data
+        for battery in history:
+            ax.plot(history[battery][0], history[battery][1],
+                    label=battery, marker='o')
+        ax.set_xlabel("Date")
+        ax.set_ylabel("Charge")
+        self.figure.autofmt_xdate()
+        self.figure.legend()
+
+        # refresh canvas
+        self.canvas.draw()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Battmon - A simple battery monitoring tool for Linux-based operating systems")
     parser.add_argument("--log", action="store_true", help="log current battery charge and status")
-    parser.add_argument("--graph", action="store_true", help="graph battery history")
+    parser.add_argument("--graph", action="store_true", help="show battery history graph GUI")
     args = parser.parse_args()
-    
+
     if args.log:
         log_battery_state()
     if args.graph:
-        graph_battery()
+        app = QtGui.QApplication(sys.argv)
+        main = Window()
+        main.show()
+        sys.exit(app.exec_())
     if (not args.log and not args.graph):
         parser.print_help()
-
-
-
-
