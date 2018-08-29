ee9d19756cfb9f4be5e9acc5b6b22abdb3dffd67
Umesh Padia <padiauj@users.noreply.github.com>
2018-08-29 14:59:09 -0400

    Removed pyqt4 from setup.py (can't be installed from pypi). PPA release now stable.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import subprocess
 
 setup(
     name = "battmon",
-    version = "1.2.0",
+    version = "1.2.1",
     author = "Umesh Padia",
     author_email = "umeshpadia1@gmail.com",
     description = "A simple battery monitoring tool for Linux operating systems",
@@ -26,6 +26,5 @@ setup(
         "setuptools",
         "matplotlib",
         "numpy",
-        "pyqt4",
     ],
 )
